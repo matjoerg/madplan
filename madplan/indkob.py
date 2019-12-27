92f9b5acd0b0e363ffe79f023b1be90433fad18e
Mathias S. Jørgensen <matjoerg.dk@gmail.com>
2019-12-27 20:14:49 +0100

    toward console app

--- madplan/indkob.py
+++ madplan/indkob.py
@@ -2,7 +2,21 @@ class Indkobsliste:
     """ """
 
     def __init__(self):
-        alle_ingredienser = []
-        
-    def lav_liste(self, ugeplan):
+        self.alle_ingredienser = []
+
+    def saml_alle_ingredienser(self, ugeplan):
+        for ret in ugeplan.retter:
+            for ingrediens in ret.ingredienser:
+                found_duplicate = False
+                for index, other_ingrediens in enumerate(self.alle_ingredienser):
+                    if ingrediens == other_ingrediens:
+                        found_duplicate = True
+                        self.alle_ingredienser[index].antal += ingrediens.antal
+
+                if not found_duplicate:
+                    self.alle_ingredienser.append(ingrediens)
+
+        return self.alle_ingredienser
+
+    def __str__(self):
         
